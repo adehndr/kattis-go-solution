f1edd7521d71e0732acd1246a63a1141642ec31d
adehndr <56023937+adehndr@users.noreply.github.com>
2023-11-07 20:52:07 +0700

    feat: added bugged mertyu solution

---
+++ map/mertyu/mertyu.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	bruteForceSolution()
+}
+
+func bruteForceSolution() {
+	aMap := map[string]string{
+		"1":  "`",
+		"2":  "1",
+		"3":  "2",
+		"4":  "3",
+		"5":  "4",
+		"6":  "5",
+		"7":  "6",
+		"8":  "7",
+		"9":  "8",
+		"0":  "9",
+		"-":  "0",
+		"=":  "-",
+		"W":  "Q",
+		"E":  "W",
+		"R":  "E",
+		"T":  "R",
+		"Y":  "T",
+		"U":  "Y",
+		"I":  "U",
+		"O":  "I",
+		"P":  "O",
+		"[":  "P",
+		"]":  "[",
+		"\\": "]",
+		"S":  "A",
+		"D":  "S",
+		"F":  "D",
+		"G":  "F",
+		"H":  "G",
+		"J":  "H",
+		"K":  "J",
+		"L":  "K",
+		";":  "L",
+		"'":  ";",
+		"X":  "Z",
+		"C":  "X",
+		"V":  "C",
+		"B":  "V",
+		"N":  "B",
+		"M":  "N",
+		",":  "M",
+		".":  ",",
+		"/":  ".",
+		" ":  " ",
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	var result strings.Builder
+	for _, chr := range line {
+		result.WriteString(aMap[string(chr)])
+	}
+	fmt.Print(result.String()," ")
+}
